ba9db81502c2c65aea2282bb8f366f20dd7943d1
till-olelohse <till-ole.lose@tum.de>
2023-06-30 18:33:52 +0200

    fixed main handling of -h and -help (please do not use spaces between height and width!)

--- Implementierung/main.c
+++ Implementierung/main.c
@@ -158,6 +158,7 @@ int main(int argc, char *argv[]) {
             if (argLen >= extLen && strcmp(arg + argLen - extLen, extension) == 0) {
                 input = argv[i];
                 i += 1;
+                continue;
             } else {
                 printf("Fehler: Falsches Dateiformat angegeben");
                 return 0;
@@ -166,8 +167,8 @@ int main(int argc, char *argv[]) {
 
     }
 
-    input = "/Users/till-olelohse/CLionProjects/pixelwiederholung/Implementierung/4_pixels.bmp";
-    output = "/Users/till-olelohse/CLionProjects/pixelwiederholung/Implementierung/4_pixels_output.bmp";
+    // input = "/Users/till-olelohse/CLionProjects/pixelwiederholung/Implementierung/4_pixels.bmp";
+    // output = "/Users/till-olelohse/CLionProjects/pixelwiederholung/Implementierung/4_pixels_output.bmp";
 
     window_test(input, output, x, y, width, height, scale
     );
