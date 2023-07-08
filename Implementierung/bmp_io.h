55d71469c24a046e541d236cd49f36ff70ec118f
Laert Llaveshi <llaveshl@in.tum.de>
2023-07-08 15:00:57 +0200

    restructuring again

--- Implementierung/bmp_io.h
+++
@@ -1,9 +0,0 @@
-#ifndef BMP_IO_H
-#define BMP_IO_H
-
-#include <stdint.h>
-#include <stddef.h>
-
-void pixelwiederholung(const char *input, const char *output, size_t x, size_t y, size_t width, size_t height, size_t scale);
-
-#endif
