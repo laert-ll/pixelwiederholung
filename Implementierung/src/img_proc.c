3dd68a2ebd02d81b6acbf3ee5fdb2dde68ebf689
Laert Llaveshi <llaveshl@in.tum.de>
2023-07-11 17:32:50 +0200

    horizontale Lücken

--- Implementierung/src/img_proc.c
+++ Implementierung/src/img_proc.c
@@ -29,18 +29,20 @@ void zoom(const uint8_t *img, size_t width, size_t height, size_t scale, uint8_t
     size_t maxHeight = zoomedHeight + zoomedWidth;
     // Anzahl von zu kopierenden Pixeln
     size_t length = width*height;
-    // Wo die zu kopierenden Pixeln gespeichert werden
-    size_t temp = 0;
     // Anzahl an Layers von Nachbarn
     int radius = (scale - 1) / 2;
+    // Array wo die Adressen aller Ursprungspixel gespeichert werden
+    size_t *pixelAddr = malloc(length*sizeof(size_t));
+    // Wo die zu kopierenden Pixeln gespeichert werden
+    size_t temp = 0;
+    // Nützliche temporäre index
     size_t index = 0;
-    size_t *fixpointAddr = malloc(length*sizeof(size_t));
 
     // Kopieren aller Originalpixel
     for (size_t y = 0; y < height; y++) {
         for (size_t x = 0; x < width; x++) {
-            fixpointAddr[index++] = zoomedHeight - y * zoomedWidth * scale + x * 3 * scale;
-            uint8_t* target_ptr = result + fixpointAddr[index-1];
+            pixelAddr[index++] = zoomedHeight - y * zoomedWidth * scale + x * 3 * scale;
+            uint8_t* target_ptr = result + pixelAddr[index-1];
             uint8_t* source_ptr = img + ((height * paddedWidth - paddedWidth) - y * paddedWidth + x * 3);
             target_ptr[0] = source_ptr[0];
             target_ptr[1] = source_ptr[1];
@@ -51,7 +53,7 @@ void zoom(const uint8_t *img, size_t width, size_t height, size_t scale, uint8_t
     // Nachbarn kopieren
     for (size_t t = 0; t < length; t++) {
         // Pixel das kopiert sein soll
-        temp = fixpointAddr[t];
+        temp = pixelAddr[t];
         for (int i = -1*radius; i <= radius; i++) {
             // Falls Pixel auf rechte seite steht, soll bissn rechter anfangen
             if (t % width) index = 0;
@@ -68,32 +70,37 @@ void zoom(const uint8_t *img, size_t width, size_t height, size_t scale, uint8_t
     if (scale % 2) index = 0;
     else index = 1;
 
-    // Falls Lücken
-    if (index) {
-        // Vertikale Lücken für die erste Reihe
-        temp = fixpointAddr[0];
-        for (int i = -1*radius; i <= 0; i++)
-                memcpy(result + temp + (radius + 1)*3 + zoomedWidth*i, result + temp, sizeof(uint8_t) * 3);
-        // Andere vertikale Lücken
-        for (size_t t = width; t < length; t++) {
-            temp = fixpointAddr[t];
-            for (int i = -1*radius; i <= radius; i++)
-                    memcpy(result + temp + (radius + 1)*3 + zoomedWidth*i, result + temp, sizeof(uint8_t) * 3);
-        }
-        // TODO: Horizonatle Lücken
-    }
-
     // Recher Rand
     for (size_t t = width - 1; t < length; t+=width) {
-        temp = fixpointAddr[t];
+        temp = pixelAddr[t];
         for (int i = -1*radius; i <= radius; i++)
             memcpy(result + temp + (radius+1)*3 + i*zoomedWidth, result + temp, sizeof(uint8_t) * 3 * (radius + index));
     }
 
+    // Falls Lücken
+    if (index) {
+        // Vertikale Lücken für die erste Reihe
+        temp = pixelAddr[0];
+        for (int i = -1*radius; i <= 0; i++)
+                memcpy(result + temp + (radius + 1)*3 + zoomedWidth*i, result + temp, sizeof(uint8_t) * 3);
+        // Andere vertikale Lücken
+        for (size_t t = width; t < length; t++) {
+            temp = pixelAddr[t];
+            for (int i = -1*radius; i <= radius; i++)
+                    memcpy(result + temp + (radius + 1)*3 + zoomedWidth*i, result + temp, sizeof(uint8_t) * 3);
+        }
+        // Horizontale Lücken
+        for (size_t t = 0; t < length; t+=width) {
+            temp = pixelAddr[t];
+            memcpy(result + temp - zoomedWidth*(radius+1), result + temp, sizeof(uint8_t) * 3 * width * scale);
+        }
+    }
+
     // Unterer Rand
-    for (size_t t = 1; t <= radius; t++)
-        memcpy(result + fixpointAddr[length-width] - zoomedWidth*(radius+t+1), 
-                result + fixpointAddr[length-width], 
-                sizeof(uint8_t) * 3 * zoomedWidth);
+    for (size_t t = 1; t < radius + 2; t++) {
+        memcpy(result + pixelAddr[length-width] - zoomedWidth*(radius+t), 
+                result + pixelAddr[length-width], 
+                sizeof(uint8_t) * 3 * width * scale);
+    }
 
 }
